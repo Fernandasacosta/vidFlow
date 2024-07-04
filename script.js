f69178f74b4e3841e7c7aa043dc56f0fbfec029b
Fernandasacosta <107959631+Fernandasacosta@users.noreply.github.com>
2024-07-04 10:42:21 -0300

    refactor: use try catch and async await

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const conteinerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos") // fetch buscar informação e retorna a promise
 
-.then(res => res.json()) // transformar a resposta da promise em json
-.then((videos) => 
-    videos.forEach((video) =>{
-        conteinerVideos.innerHTML += `
-        <li class="videos__item"> 
-            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-            <div class="descricao-video">
-                <img class="img-canal" src="${video.imagem}  alt="Logo do canal">
-                <h3 class="titulo-video">${video.titulo}</h3>
-                 <p class="titulo-canal">${video.descricao}</p>
-            </div>
-        </li>`
-        ;
-    })
-)
+
+
+async function buscarEMostrarVideos(){
+    try{
+        const apiBusca = await fetch("http://localhost:3000/videos"); // fetch buscar informação e retorna a promise
+        const videos = await apiBusca.json(); // transformar a resposta da promise em json
+        
+        
+    
+            videos.forEach((video) =>{
+                conteinerVideos.innerHTML += `
+                <li class="videos__item"> 
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem}  alt="Logo do canal">
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                    </div>
+                </li>`
+                ;
+            })
+            //tratativa
+    } catch(error){
+        conteinerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos : ${error}</p>`
+    } 
+}
+
+buscarEMostrarVideos();
